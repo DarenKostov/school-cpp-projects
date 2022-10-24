31dbf4c0b89b3ea56d3f7c14223894756ecb1a15
Daren-Kostov <508324@bsd48.org>
2022-10-24 12:19:38 -0700

    added-music-class

--- mediaDatabase/music.cpp
+++ mediaDatabase/music.cpp
@@ -3,9 +3,19 @@
 
 using namespace std;
 
-Music::Music(){
+Music::Music(char Title[80], int Date, char Artist[80], char Duration[80], char Publisher[80]):Media(Title, Date){
+  for(int i=0; i<80; i++){
+    artist[i]=Artist[i];
+    duration[i]=Duration[i];
+    publisher[i]=Publisher[i];
+  }
+  
+  
 };
-
+void Music::printAll(){
+  cout << "Music: [Title: " << title << "; Year: " << date << "; Artist: "
+       << artist << "; Duration: " << duration << "; Publisher: " << publisher << "]\n";
+}
 
 
 
--- mediaDatabase/music.h
+++ mediaDatabase/music.h
@@ -1,17 +1,20 @@
 #include <iostream>
-#include "./media.h"
 
+#ifndef MEDIA
+#define MEDIA
+#include "./media.h"
+#endif
 using namespace std;
 
 class Music : public Media{
    public:
-      Music();
-   
-   
-   protected:
-   char artist[80];
-   int date;
-   int type;
+      Music(char title[80], int date, char artist[80], char duration[80], char Publisher[80]);
+      virtual void printAll();
+
+   private:
+      char artist[80];
+      char duration[80];
+      char publisher[80]; 
 };
 
 
