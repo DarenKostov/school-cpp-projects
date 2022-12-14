1581313532789ac4152670e5f8783d28dbec47b2
DarenKostov <daren.kostov@outlook.com>
2022-12-13 19:11:35 -0800

    Path class

---
+++ zuul/path.cpp
@@ -0,0 +1,147 @@
+/*
+  Daren Kostov
+
+*/
+#include <vector>
+
+#ifndef q
+#define q
+#include "./text.h"
+#include "./path.h"
+#endif
+
+using namespace std;
+
+Path::Path(){}
+
+Path::Path(Text input){
+  //set with the = operator for Text
+  *this=input;
+}
+
+Path::Path(vector<Text> input){
+  //set with the = operator for vector of Texts
+  *this=input;
+}
+
+//= VECTOR
+void Path::operator=(vector<Text> input){
+  //empty the path
+  while(path.size()!=0)
+    path.erase(path.begin());
+  
+  //fill the path with the new path
+  for(vector<Text>::iterator i=input.begin(); i!=input.end(); i++)
+    path.push_back(*i);
+  
+  reCalculateDepth();
+}
+
+//= TEXT
+void Path::operator=(Text input){
+  vector<Text> outputV;
+  char outputC[999][999];
+  
+  int j=0;
+  int word=0;
+  for(int i=0; i<999; i++){
+    //if the next charcter is "\" go to the next folder/file
+    if(input.val()[i]=='/'){
+      outputC[word][i-j]='\0';
+      j=++i;
+      word++;
+    }  
+    //add the char to the current folder/file
+    outputC[word][i-j]=input.val()[i];
+    
+    //if its the end of the Text break out of the loop
+    if(input.val()[i]=='\0'){
+      break;     
+    }
+  }
+  
+  //fill the output with all of the folders/files
+   for(int i=0; i<word+1; i++)
+    outputV.push_back(Text(outputC[i]));
+  
+  
+  //use the == operator for vector
+  *this=outputV;
+}
+
+//= PATH
+void Path::operator=(Path input){
+  //set with the == operator for vector
+  *this=input.wholeV();
+}
+
+
+//gets the path in Text form
+Text Path::wholeT(){
+  char slash[2]="/";
+  Text output;
+  
+  for(vector<Text>::iterator i=path.begin(); i!=path.end(); i++){
+    //the magic of using Text instead of char*
+     output+=slash;
+     output+=*i;
+  }
+  return output;
+}
+
+//get the entire vector
+vector<Text> Path::wholeV(){
+  return path;
+}
+
+//gets length
+int Path::getDepth(){
+  return depth;
+}
+
+//gets the folder/file at index i
+Text Path::atDepth(int i){
+  //dont go over the limit and cause a seg fault
+  i%=depth;
+  
+  //pointer arithmetic
+  return *(path.begin()+i);
+}
+
+//recalculates depth based on the size of the vector
+void Path::reCalculateDepth(){
+  depth=path.size();
+}
+
+//aliases
+
+Text Path::wholeText(){
+  return wholeT();
+}
+
+vector<Text> Path::wholeVector(){
+  return wholeV();
+}
+
+int Path::length(){
+  return getDepth();
+}
+
+int Path::len(){
+  return getDepth();
+}
+
+Text Path::atLength(int i){
+  return atDepth(i);
+}
+
+Text Path::atLen(int i){
+  return atDepth(i);
+}
+
+Text Path::atIndex(int i){
+  return atDepth(i);
+}
+
+
+
---
+++ zuul/path.h
@@ -0,0 +1,55 @@
+/*
+  Daren Kostov
+  a simple class that stores a path to a file/folder in the system
+*/
+
+#include <vector>
+
+#ifndef q
+#define q
+#include "./text.h"
+#endif
+
+
+class Path{
+  
+  public:
+  Path();
+  Path(Text); //initialize from Text
+  Path(std::vector<Text>); //initialize from a vecor of Texts
+  
+  
+  void operator=(Text); //set with Text
+  void operator=(Path); //set with another Path
+  void operator=(std::vector<Text>); //set from a vector of Texts containing folders
+
+  Text wholeT(); //gives you the whole path in the form of a single Text
+  std::vector<Text> wholeV(); //gives you the whole path in the form of a vector of Texts
+  int getDepth(); //gives you how deep the the path, ex /qw/er/ty/ has dept of 3
+  Text atDepth(int); //gives you the folder/file at depth i, starting from root
+
+  //function aliases
+  Text wholeText(); //gives you the whole path in the form of a Text
+  std::vector<Text> wholeVector(); //gives you the whole path in the form of vector
+  int length(); //gives you how deep the the path, ex /qw/er/ty/ has dept of 3
+  int len(); //gives you how deep the the path, ex /qw/er/ty/ has dept of 3
+  Text atLength(int); //gives you the folder/file at length i, starting from root
+  Text atLen(int); //gives you the folder/file at length i, starting from root
+  Text atIndex(int); //gives you the folder/file at index i, starting from root
+
+  private:
+  void reCalculateDepth(); //recalculates the depth of the Path
+  
+  std::vector<Text> path; //where we store the path
+  int depth; //how many layers does the path have ex. /gh/hj/jk/kl has 4
+  
+  /*
+    note:
+    want to initialize from a char*? do:
+    char char1[100]="qwerty";
+    Path a(Text(char1));
+  
+  */
+
+  
+};
