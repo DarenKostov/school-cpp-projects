c09f06e45a3a20bf25d66ae76434e7bce62a6391
DarenKostov <daren.kostov@outlook.com>
2022-12-19 15:36:35 -0800

    computer header complete

--- zuul/computer.h
+++ zuul/computer.h
@@ -17,6 +17,7 @@
 
 #include <vector>
 #include <map>
+#include <ctime>
 
 class Computer{
   public:
@@ -25,18 +26,55 @@ class Computer{
         
     //computer
     Computer();
-
+  
     //sets up the folder tree
     void setRoot(Folder);
-      
+  
+    //set up the parser
+    void setParser(Parser);
     
   
-    //parser
+    //=files
+    //deletes a file from name OR path
+    bool deleteFile(Text);
+  
+    //create a file from name OR path
+    bool createFile(File, Text);
+  
+    //get full access to file with name or path
+    File& getFile(Text);
     
+    //=folders
+    
+    //deletes a folder from name OR path
+    bool deleteFolder(Text);
+  
+    //create a folder from name OR path
+    bool createFolder(Folder, Text);
+  
+    //get full access to a folder with name or path
+    Folder& getFolder(Text);
+  
+    //=currentFolder
+    
+    //gives you acopy of the current folder
+    Folder getCurrentFolder();
+      
+    //changes your current folder to from name or path
+    bool goTo(Text);
+    
+    //the parser manages itself, no need to be private
+    //command parser
+    Parser parser;  
   
   private:
-    std::map<Path, Folder> allFolders;
-    std::map<Path, File> allFiless;
+    Folder* currentFolder;
+    
+    // root folder (not root user folder)
+    Folder root;
+  
+    std::map<Path, Folder*> allFolders;
+    std::map<Path, File*> allFiless;
   
 
 };
