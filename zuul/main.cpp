ff3450dacfb3209e022138f913442f5d58345e5c
DarenKostov <daren.kostov@outlook.com>
2022-12-22 21:04:47 -0800

    finished ls

--- zuul/main.cpp
+++ zuul/main.cpp
@@ -98,10 +98,6 @@ const Text Admin_thinking="\033[91m";
 using namespace std;
 
 
-void addAllCommands(vector<Command> &commandBank);
-void addedCommandInfo(Command command);
-
-
 
 //generates random Text with certain size
 Text randomText(int);
@@ -168,37 +164,6 @@ int main(){
 }
 
 
-//==commands from user
-void execCD(Computer& inComp){
-   if(inComp.goTo(inComp.parser.returnCommandT(1)))
-      execLS(inComp);
-}
-void execLS(Computer& inComp){
-      fasttalk(inComp.on,inComp.getCurrentFolder().allItems(), Info);
-}
-   
-//vector object pointers
-template <class T>
-void  emptyVector(vector<T*> &toBeEmptied){
-   //free up the memory used by the objects in the vector
-   for(typename vector<T*>::iterator i=toBeEmptied.begin(); i!=toBeEmptied.end(); i++){
-      delete (*i);
-   }
-   //delete all of the pointers in the vector
-   while(toBeEmptied.size()!=0){
-      toBeEmptied.erase(toBeEmptied.begin());  
-   } 
-}
-//vector of objects
-template <class T>
-void  emptyVector(vector<T> &toBeEmptied){
-   //delete all of the pointers in the vector
-   while(toBeEmptied.size()!=0){
-      toBeEmptied.erase(toBeEmptied.begin());  
-   } 
-}
-
-
 Text randomText(int size){
    Text out;
       for(int i=0; i<size; i++)
@@ -214,151 +179,56 @@ Text randomTextNumbers(int size){
 
 
 
-void addedCommandInfo(Command command){
-  cout << "=command name: "<< command.aliases[0].val() << "\n=command description:\n";
-  cout << command.description.val() << "\n=includes these aliases:\n";
-  for(int i=0; i<command.aliasesAmount; i++)
-     cout << command.aliases[i].val() << ", ";
-  cout << "\n=these arguments:\n" << command.argsDescription.val() << endl;
-  cout <<  "=argument types:\n";
-  for(int i=0; i<command.argsAmount; i++)
-     cout << command.args[i].val() << ", ";
-  cout << endl;
-}   
-
-
-//all commands to be added
-void addAllCommands(vector<Command> &commandBank){
-   //big brain {} block to encapsulate variables
-   {
-   char alias[100][100]={"help", "h"};
-   char description[100]="Prints this help.";
-   char args[100][100]={"cmd"};
-   char argsDescription[100]="none";
-   commandBank.push_back( Command(2, alias, description, 1, args, argsDescription));
-   }
-   {
-   char alias[100][100]={"cd", "chgdir", "cngdir", "chngdir"};
-   char description[100]="Changes the current directory.";
-   char args[100][100]={"cmd", "txt"};
-   char argsDescription[100]="<directory path>";
-   commandBank.push_back( Command(4, alias, description, 2, args, argsDescription));
-   }
-   {
-   char alias[100][100]={"copy", "c"};
-   char description[100]="Copies a file into the ram.";
-   char args[100][100]={"cmd", "txt"};
-   char argsDescription[100]="<source path>";
-   commandBank.push_back( Command(2, alias, description, 2, args, argsDescription));
-   }
-   {
-   char alias[100][100]={"paste", "p"};
-   char description[100]="pastes all files from ram. OR pastes a specifc file";
-   char args[100][100]={"cmd", "txt", "txt"};
-   char argsDescription[100]="<destination path> [file id]";
-   commandBank.push_back( Command(2, alias, description, 2, args, argsDescription));
-   }
-   {
-   char alias[100][100]={"cp", "copypaste"};
-   char description[100]="copies a file into memory and pastes it.";
-   char args[100][100]={"cmd", "txt", "txt"};
-   char argsDescription[100]="<source path> <destination path>";
-   commandBank.push_back( Command(2, alias, description, 3, args, argsDescription));
-   }
-   {
-   char alias[100][100]={"rm", "remove"};
-   char description[100]="Deletes a file.";
-   char args[100][100]={"cmd", "txt"};
-   char argsDescription[100]="<file path>";
-   commandBank.push_back( Command(2, alias, description, 2, args, argsDescription));
-   }
-   {
-   char alias[100][100]={"man", "manual"};
-   char description[100]="Shows detals on how to use a program.";
-   char args[100][100]={"cmd", "txt"};
-   char argsDescription[100]="<program>";
-   commandBank.push_back( Command(2, alias, description, 2, args, argsDescription));
-   }
-   {
-   char alias[100][100]={"free", "fr"};
-   char description[100]="Shows the ram usage.";
-   char args[100][100]={"cmd", "txt"};
-   char argsDescription[100]="<flag>";
-   commandBank.push_back( Command(2, alias, description, 2, args, argsDescription));
-   }
-   {
-   char alias[100][100]={"mv", "move"};
-   char description[100]="Moves a file.";
-   char args[100][100]={"cmd", "txt", "txt"};
-   char argsDescription[100]="<source path> <destination path>";
-   commandBank.push_back( Command(2, alias, description, 3, args, argsDescription));
-   }
-   {
-   //Table Of Processes, i know it doesn't stand for that, but makes more sense as an alieas than "top CPU consumers"
-   char alias[100][100]={"top", "tbloprc", "tbloprs"};
-   char description[100]="Shows process's name, memory usage, and cpu usage";
-   char args[100][100]={"cmd"};
-   char argsDescription[100]="none";
-   commandBank.push_back( Command(3, alias, description, 1, args, argsDescription));
-   }
-   {
-   //doesnt copy to the buffer, its the buffer thats copied into
-   char alias[100][100]={"cb", "copybuffer", "copybank"};
-   char description[100]="Shows you what files you have in the copy buffer.";
-   char args[100][100]={"cmd"};
-   char argsDescription[100]="none";
-   commandBank.push_back( Command(3, alias, description, 1, args, argsDescription));
+//==commands from user
+void execCD(Computer& inComp){
+   if(inComp.goTo(inComp.parser.returnCommandT(1)))
+      execLS(inComp);
+}
+void execLS(Computer& inComp){
+   char spacer='\t';
+   
+   //long listing format
+   bool longList=false;
+   
+   Text flags=inComp.parser.returnCommandT(1);
+   //take in flags only if they are prefixed with a dash
+   if(flags[0]=='-'){
+      //if any l
+      if(flags[1]=='l' || flags[2]=='l')
+         longList=true;
+      //if 1
+      else if(flags[1]=='1')
+         spacer='\n';  
    }
    
-   {
-   char alias[100][100]={"ssh", "secureshell"};
-   char description[100]="Secure Shell";
-   char args[100][100]={"cmd", "txt"};
-   char argsDescription[100]="<who>@<adress>\n<adress>";
-   commandBank.push_back( Command(2, alias, description, 2, args, argsDescription));
-   }
-   {
-   char alias[100][100]={"scp", "securecopy"};
-   char description[100]="Secure, Contain, Protect!\n\nAhem, SSH secure file copy";
-   char args[100][100]={"cmd", "txt", "txt"};
-   char argsDescription[100]="<who>@<adress>:<source path> <who>@<adress>:<destination path>";
-   commandBank.push_back( Command(2, alias, description, 3, args, argsDescription));
-   }
-   {
-   char alias[100][100]={"cat", "catout"};
-   char description[100]="Concatenate files and print on the standard output. tl;dr Print files into terminal.";
-   char args[100][100]={"cmd", "txt", "txt"};
-   char argsDescription[100]="<file path>";
-   commandBank.push_back( Command(2, alias, description, 2, args, argsDescription));
-   }
-   {
-   char alias[100][100]={"grep"};
-   char description[100]="Print lines from a file that match a pattern.";
-   char args[100][100]={"cmd", "txt", "txt"};
-   char argsDescription[100]="<pattern> <file path>";
-   commandBank.push_back( Command(2, alias, description, 3, args, argsDescription));
-   }
-   {
-   char alias[100][100]={"tree"};
-   char description[100]="Print a tree of all child folders/files";
-   char args[100][100]={"cmd"};
-   char argsDescription[100]="none";
-   commandBank.push_back( Command(1, alias, description, 1, args, argsDescription));
-   }
-   {
-   char alias[100][100]={"exit", "quit"};
-   char description[100]="Exits the game";
-   char args[100][100]={"cmd"};
-   char argsDescription[100]="none";
-   commandBank.push_back( Command(2, alias, description, 1, args, argsDescription));
-   }
-   {
-   char alias[100][100]={"ls", "list"};
-   char description[100]="Lists the items in the current directory";
-   char args[100][100]={"cmd"};
-   char argsDescription[100]="none";
-   commandBank.push_back( Command(2, alias, description, 1, args, argsDescription));
-   }
+   
+   
+   
+   //all Folders
+   auto allFold=inComp.getCurrentFolder().allFolders();
+   for(auto i=allFold.begin(); i!=allFold.end(); i++)
+      if(longList){
+         fasttalk(inComp.on, (*i)->getDate(), folderColor);
+         fasttalk(inComp.on, ' '+(*i)->path.wholeT()+'\n', folderColor);
+      }else
+         fasttalk(inComp.on, (*i)->name() +spacer, folderColor);
+   
+   
+   
+   //all Files
+   auto allFile=inComp.getCurrentFolder().allFiles();
+   for(auto i=allFile.begin(); i!=allFile.end(); i++)   
+      if(longList){
+         fasttalk(inComp.on, (*i)->getDate(), folderColor);
+         fasttalk(inComp.on, ' '+(*i)->path.wholeT()+'\n', folderColor);
+      }else
+         fasttalk(inComp.on, (*i)->name() +spacer, folderColor);
+   
+   fasttalk(inComp.on, Text("\n"), Info);
 }
+   
+
+
+
 
 
