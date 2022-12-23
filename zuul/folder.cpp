adcc02ea0551149896ac808dac6dfc8b7d31f377
DarenKostov <daren.kostov@outlook.com>
2022-12-22 18:34:52 -0800

    rewrote the folder class to work with pointers

--- zuul/folder.cpp
+++ zuul/folder.cpp
@@ -44,11 +44,15 @@ time_t Folder::getDate(){
   return date;
 }
 
+//==file
+
 bool Folder::deleteFile(Text name){
-  for(vector<File>::iterator i=files.begin(); i!=files.end(); i++){
+  for(auto i=files.begin(); i!=files.end(); i++){
     //is the file there, if so remove it
-    if(strcmp(name.val(), i->path.name().val())==0){
+    if(strcmp(name.val(), (*i)->path.name().val())==0){
+      delete *i;
       files.erase(i);
+      //update time, we modified the folder
       date=time(nullptr);
       return true;
     }
@@ -62,33 +66,64 @@ bool Folder::deleteFile(const char* name){
   return deleteFile(Text(name));
 }
 
-bool Folder::addFile(File in){
+bool Folder::addFile(File* in){
   
   //is there a file with the same name? if so dont add it
   for(int  i=0; i<files.size(); i++){
-    if(strcmp(in.name().val(), files[i].path.name().val())==0){
+    if(strcmp(in->name().val(), files[i]->path.name().val())==0){
       return false;
     }
   }
   
   //is there a folder with the same name? if so dont add it
   for(int  i=0; i<folders.size(); i++){
-    if(strcmp(in.name().val(), folders[i].path.name().val())==0){
+    if(strcmp(in->name().val(), folders[i]->path.name().val())==0){
       return false;
     }
   }
   
   files.push_back(in);
   updatePaths();
+  //update time, we modified the folder
+  date=time(nullptr);
   return true;
 }
 
 
+vector<File*> Folder::allChildFiles(){
+  vector<File*> output;
+  
+  for(auto i=files.begin(); i!=files.end(); i++)
+    //adress of the derefrenced file iterator
+    output.push_back(*i);
+  
+  for(auto i=folders.begin(); i!=folders.end(); i++){
+    //adress of the derefrenced folder iterator
+    // output.push_back(&*i);
+    
+    //get the children's files
+    vector<File*> childFiles=(*i)->allChildFiles(); 
+    
+    //add them to output
+    for(auto j=childFiles.begin(); j!=childFiles.end(); j++)
+      output.push_back(*j);
+  }
+  
+  return output;
+}
+
+
+
+//==folder
+
 bool Folder::deleteFolder(Text name){
-  for(vector<Folder>::iterator i=folders.begin(); i!=folders.end(); i++){
+  for(auto i=folders.begin(); i!=folders.end(); i++){
     //is the file there, if so remove it
-    if(strcmp(name.val(), i->path.name().val())==0){
+    if(strcmp(name.val(), (*i)->path.name().val())==0){
+      (*i)->emptyMe();
+      delete *i;
       folders.erase(i);
+      //update time, we modified the folder
       date=time(nullptr);
       return true;
     }
@@ -102,45 +137,70 @@ bool Folder::deleteFolder(const char* name){
   return deleteFile(Text(name));
 }
 
-bool Folder::addFolder(Folder in){
+bool Folder::addFolder(Folder* in){
   
   //is there a file with the same name? if so dont add it
   for(int  i=0; i<files.size(); i++){
-    if(strcmp(in.name().val(), files[i].path.name().val())==0){
+    if(strcmp(in->name().val(), files[i]->path.name().val())==0){
       return false;
     }
   }
   
   //is there a folder with the same name? if so dont add it
   for(int  i=0; i<folders.size(); i++){
-    if(strcmp(in.name().val(), folders[i].path.name().val())==0){
+    if(strcmp(in->name().val(), folders[i]->path.name().val())==0){
       return false;
     }
   }
   
   folders.push_back(in);
   updatePaths();
+  //update time, we modified the folder
+  date=time(nullptr);
   return true;
 }
 
-vector<File> Folder::allFiles(){
+vector<File*> Folder::allFiles(){
   return files;
 }
   
-vector<Folder> Folder::allFolders(){
+vector<Folder*> Folder::allFolders(){
   return folders;
 }
+
+
+vector<Folder*> Folder::allChildFolders(){
+  vector<Folder*> output;
+  
+  for(auto i=folders.begin(); i!=folders.end(); i++){
+    
+    //add the children
+    //adress of the derefrenced folder iterator
+    output.push_back(*i);
+    
+    //get the childrens's folders
+    vector<Folder*> childFolders=(*i)->allChildFolders(); 
+    
+    //add them to output
+    for(auto j=childFolders.begin(); j!=childFolders.end(); j++)
+      output.push_back(*j);
+  }
+  
+  return output;
+}
+
+
 
 Text Folder::allItems(){
   Text output="files:\n";
   
   for(int i=0; i<files.size(); i++)
-      output+=files[i].name()+'\n';
+      output+=files[i]->name()+'\n';
   
   output+="folders:\n";  
   
   for(int i=0; i<folders.size(); i++)
-    output+=folders[i].name()+'\n';
+    output+=folders[i]->name()+'\n';
 
   return output;
 }
@@ -151,11 +211,11 @@ long Folder::usage(){
   
   //files
   for(int i=0; i<files.size(); i++)
-    out+=files[i].usage();
+    out+=files[i]->usage();
   
   //folders, this will eterate through all child folders, and the children of those, etc until only files are left
   for(int i=0; i<folders.size(); i++)
-    out+=folders[i].usage();
+    out+=folders[i]->usage();
 
   //a folder is bytes
   return out+8;
@@ -164,12 +224,12 @@ long Folder::usage(){
 
 void Folder::updatePaths(){
 for(int i=0; i<files.size(); i++)
-    files[i].path.changeParentOnly(path);
+    files[i]->path.changeParentOnly(path);
   
   //folders, this will eterate through all child folders, and the children of those, etc until only files are left
   for(int i=0; i<folders.size(); i++){  
-    folders[i].path.changeParentOnly(path);
-    folders[i].updatePaths();
+    folders[i]->path.changeParentOnly(path);
+    folders[i]->updatePaths();
   }
   
   //paths updated
@@ -177,46 +237,6 @@ for(int i=0; i<files.size(); i++)
 }
 
 
-vector<File*> Folder::allFilesAdr(){
-  vector<File*> output;
   
-  for(auto i=files.begin(); i!=files.end(); i++)
-    //adress of the derefrenced file iterator
-    output.push_back(&*i);
-  
-  for(auto i=folders.begin(); i!=folders.end(); i++){
-    //adress of the derefrenced folder iterator
-    // output.push_back(&*i);
-    
-    //get the children's files
-    vector<File*> childFiles=i->allFilesAdr(); 
-    
-    //add them to output
-    for(auto j=childFiles.begin(); j!=childFiles.end(); j++)
-      output.push_back(*j);
-  }
-  
-  return output;
-}
-  
-vector<Folder*> Folder::allFoldersAdr(){
-  vector<Folder*> output;
-  
-  for(auto i=folders.begin(); i!=folders.end(); i++){
-    
-    //add the children
-    //adress of the derefrenced folder iterator
-    output.push_back(&*i);
-    
-    //get the childrens's folders
-    vector<Folder*> childFolders=i->allFoldersAdr(); 
-    
-    //add them to output
-    for(auto j=childFolders.begin(); j!=childFolders.end(); j++)
-      output.push_back(*j);
-  }
-  
-  return output;
-}
 
 
--- zuul/folder.h
+++ zuul/folder.h
@@ -2,8 +2,6 @@
   Daren Kostov
   a class that stores other folders or files
 
-  perhaps I should make the folder store folder pointers and not the whole folders
-  since im wasting memory storing duplicte folders however parent folders they have
 
 */
 #ifndef q
@@ -23,41 +21,54 @@ class Folder{
   //Folder with a Path
   Folder(Path);
 
-  //set the folder to another folder
   Folder& operator=(Folder);
-  
+
   //gets the name of the folder
   Text name();
   
   //gets when the folder was last modified
   std::time_t getDate();
   
+  
+  //=file
+  
   //deletes a File with name, returns false if unsuccessful
   bool deleteFile(Text);
   bool deleteFile(const char*);
   
   //adds a File, deletes false if unsuccessful
-  bool addFile(File);
+  bool addFile(File*);
+  
+  //returns all files in this Folder, a copy
+  std::vector<File*> allFiles();
+  
+  //returns all file adresses in this Folder and its children
+  std::vector<File*> allChildFiles();
+  
+  //searches for a File in any of its children given the path, recursive
+  File* findFileInChildren(Path);
+  
+  
+  //=folder
   
   //deletes a Folder with name, uses recursion, returns false if unsuccessful
   bool deleteFolder(Text);
   bool deleteFolder(const char*);
   
   //adds a Folder, deletes false if unsuccessful
-  bool addFolder(Folder);
-  
-  //returns all files in this Folder, a copy
-  std::vector<File> allFiles();
+  bool addFolder(Folder*);
   
   //returns all folders in this Folder, a copy
-  std::vector<Folder> allFolders();
-  
-  
-  //returns all file adresses in this Folder and its children
-  std::vector<File*> allFilesAdr();
+  std::vector<Folder*> allFolders();
   
   //returns all folder adresses in this Folder and its children
-  std::vector<Folder*> allFoldersAdr();
+  std::vector<Folder*> allChildFolders();
+  
+    //searches for a File in any of its children given the path, recursive
+  Folder* findFolderInChildren(Path);
+  
+  // erases everything from this folder, no questions asked
+  void emptyMe();
   
   //return all files/folder in Text form
   Text allItems();
@@ -69,8 +80,8 @@ class Folder{
   Path path;
 
   private:
-  std::vector<File> files;
-  std::vector<Folder> folders;
+  std::vector<File*> files;
+  std::vector<Folder*> folders;
   void updatePaths();
   std::time_t date;
   
