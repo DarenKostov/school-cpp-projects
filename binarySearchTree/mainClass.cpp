1158320598970b5993ca44a85b5afe679c4ed9f4
DarenKostov <daren.kostov@outlook.com>
2023-04-03 12:17:20 -0700

    some fixes

--- binarySearchTree/mainClass.cpp
+++ binarySearchTree/mainClass.cpp
@@ -34,6 +34,10 @@ void displayLeft(BinNode<int>*, BinNode<int>*, Text);
 void displayRight(BinNode<int>*, Text);
 void print(BinNode<int>*);
 
+//tells you whether or not a number is in the bin tree 
+void doesItContain(BinNode<int>*, std::vector<Text>);
+int doesItContain(BinNode<int>*, int);
+
 void MainClass::startProgram(){
   
 
@@ -42,15 +46,17 @@ void MainClass::startProgram(){
 
   // std::cout << "press \"a\" and then Enter, this is not a bug, just press \"a\" and then Enter";
   std::cout << "[BinTree]: ";
-  for(std::vector<Text> commands=readLine(); commands[0]!="quit"; commands=readLine()){
+  for(std::vector<Text> commands=readLine(); (commands[0]!="q") && (commands[0]!="quit"); commands=readLine()){
   
 
-    if(commands[0][0]=='a'){
+    if(commands[0]=="a" || commands[0]=="add"){
       addToBinTree(root, commands);
-    }else if(commands[0][0]=='d'){
+    }else if(commands[0]=="d" || commands[0]=="display"){
       display(root);
-    }else if(commands[0][0]=='p'){
+    }else if(commands[0]=="p" || commands[0]=="print"){
       print(root);
+    }else if(commands[0]=="c" || commands[0]=="contains?"){
+      doesItContain(root, commands);
     }else{
       std::cout << "\e[91m????\e[0m\n";
     }
@@ -74,6 +80,9 @@ void MainClass::startProgram(){
 
 void addToBinTree(BinNode<int>*& root, std::vector<Text> commands){
 
+  //not enough arguments
+  if(commands.size()<2)
+    return;
 
   int numToAdd=0;
 
@@ -181,11 +190,10 @@ void displayRight(BinNode<int>* current, Text inheritance){
   inheritance+="  ";
 
 
-    
 
   //print our value
-  std::cout << "r";
-  std::cout << current->getValue() << "\n";
+  std::cout << "\e[91mr";
+  std::cout << current->getValue() << "\e[0m\n";
 
   
     
@@ -230,8 +238,8 @@ void displayLeft(BinNode<int>* current, BinNode<int>* previous, Text inheritance
 
 
   //print our value
-  std::cout << "l";
-  std::cout << current->getValue() << "\n";
+  std::cout << "\e[96ml";
+  std::cout << current->getValue() << "\e[0m\n";
   
   displayLeft(current->getLeft(), current, inheritance);
   displayRight(current->getRight(), inheritance);
@@ -249,4 +257,43 @@ void print(BinNode<int>* current){
 
 
 
+void doesItContain(BinNode<int>* root, std::vector<Text> commands){
+  
+  //not enough arguments
+  if(commands.size()<2)
+    return;
+
+  int num=0;
+
+  //is this a valid number
+  try{
+    num=std::stoi(commands[1].val());
+  }catch(const std::invalid_argument& ia){
+    //invalid?
+    std::cout << "\e[91mNo, the Binary Tree defenetly does not contain \""+commands[1]+"\"\n\e[0m";
+    return;
+  }
+
+  //valid?
+  int occurances=doesItContain(root, num);
+
+  if(occurances==1)
+    std::cout << "There is 1 occurance of "+commands[1]+'\n';
+  else
+    std::cout << "There are " << occurances << " occurances of "+commands[1]+'\n';
+
+}
+
+
+
+
+int doesItContain(BinNode<int>* current, int num){
+  if(current==nullptr)
+    return 0;
+  
+  if(current->getValue()==num){
+    return doesItContain(current->getLeft(), num)+doesItContain(current->getRight(), num)+1;
+  }
+  return doesItContain(current->getLeft(), num)+doesItContain(current->getRight(), num);
+}
 
